3d274912646a36a6340b3e85f0a8dcfda4a91a64
Matthias Geisler <matthias@semanticdreamer.com>
2012-06-30 03:32:33 +0200

    working hello world

--- index.php
+++ index.php
@@ -1,8 +1,34 @@
 <?php
-require 'Slim/Slim.php';
+require './Slim/Slim/Slim.php';
+//With default settings
 $app = new Slim();
+
+//With custom settings
+$app = new Slim(array(
+    'log.enable' => true,
+    'log.path' => './logs',
+    'log.level' => 4
+));
+
+//GET route
 $app->get('/hello/:name', function ($name) {
-    echo "Hello, $name!";
+    echo "Hello, $name";
 });
+
+//POST route
+$app->post('/person', function () {
+    //Create new Person
+});
+
+//PUT route
+$app->put('/person/:id', function ($id) {
+    //Update Person identified by $id
+});
+
+//DELETE route
+$app->delete('/person/:id', function ($id) {
+    //Delete Person identified by $id
+});
+
 $app->run();
 ?>
