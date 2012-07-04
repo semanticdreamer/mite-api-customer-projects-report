463bb0a75259e83f8740d6a126aa45e672de93b1
Matthias Geisler <matthias@semanticdreamer.com>
2012-07-04 22:58:46 +0200

    added home nav btn in bar

--- app/routes/account_project.php
+++ app/routes/account_project.php
@@ -1,11 +1,14 @@
 <?php
 //GET route for project by id
 $app->get('/accounts/:accountid/projects/:projectid/', function ($accountid, $projectid) use ($app, $config, $api) {
+	$miteCustomerName = $config['accounts'][$accountid]['mite_customer_name'];
 	$project = json_decode($api->sendGetRequest('/projects/'.$projectid.'.json'), true);
 	$time_entries = json_decode($api->sendGetRequest('/time_entries.json?project_id='.$projectid), true);
 	$data = array(
 		'brand' => $config['site']['title'],
 		'project' => $project,
+		'account_name' => $miteCustomerName,
+		'account_id' => $accountid,
 		'time_entries' => $time_entries);
 	$app->render('project.twig', $data);
 })->name('project');
