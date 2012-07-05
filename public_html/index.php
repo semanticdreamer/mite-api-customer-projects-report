cb58ca460b1720bb381c1af2057c3227d8f9b65d
Matthias Geisler <matthias@semanticdreamer.com>
2012-07-05 21:17:44 +0200

    added app-wide route condition to validate accountid against config.yml

--- public_html/index.php
+++ public_html/index.php
@@ -55,6 +55,12 @@ require APPDIR.'lib/mite/api.php';
 $api = Mite_Api::getInstance();
 $api->init($config['mite']['account'], $config['mite']['api_key']);
 
+// app-wide route conditions
+$accountIdConditionRegex = '(' . implode('|', array_keys($config['accounts'])) . ')';
+Slim_Route::setDefaultConditions(array(
+    'accountid' => $accountIdConditionRegex
+));
+
 //routes using $app, $config, $api 
 require APPDIR.'routes/account.php';
 require APPDIR.'routes/account_project.php';
