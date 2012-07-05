cca2edef09d4b41c06a1ea57ce66c34ffab0cf78
Matthias Geisler <matthias@semanticdreamer.com>
2012-07-05 14:39:30 +0200

    added output escaping for text fields, added nl2br for text fields, added customer.note to account

--- app/routes/account.php
+++ app/routes/account.php
@@ -11,10 +11,12 @@ $app->get('/accounts/', function () use ($app, $config, $api) {
 $app->get('/accounts/:accountid/', function ($accountid) use ($app, $config, $api) {
 	$miteCustomerName = $config['accounts'][$accountid]['mite_customer_name'];
 	$informalSalutation = $config['accounts'][$accountid]['informal_salutation'];
+	$customer = json_decode($api->sendGetRequest('/customers.json?name='.urlencode($miteCustomerName)), true);
 	$data = array(
 		'brand' => $config['site']['title'],
 		'account_name' => $miteCustomerName,
 		'account_salutation' => $informalSalutation,
+		'account' => $customer,
 		'account_id' => $accountid);
 	$app->render('account.twig', $data);
 })->name('account');
