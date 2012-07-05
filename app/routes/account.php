5c121d766b439402f976f55c47569d08841911b2
Matthias Geisler <matthias@semanticdreamer.com>
2012-07-05 22:28:59 +0200

    added collapsable nav for projects of given account

--- app/routes/account.php
+++ app/routes/account.php
@@ -1,6 +1,7 @@
 <?php
 //GET route for accounts
 $app->get('/accounts/', function () use ($app, $config, $api) {
+	$projects = json_decode($api->sendGetRequest('/projects.json'), true);
 	$data = array(
 		'brand' => $config['app']['title'],
 		'accounts' => $config['accounts']);
@@ -12,12 +13,16 @@ $app->get('/accounts/:accountid/', function ($accountid) use ($app, $config, $ap
 	$miteCustomerName = $config['accounts'][$accountid]['mite_customer_name'];
 	$informalSalutation = $config['accounts'][$accountid]['informal_salutation'];
 	$customer = json_decode($api->sendGetRequest('/customers.json?name='.urlencode($miteCustomerName)), true);
+	$projects = json_decode($api->sendGetRequest('/projects.json'), true);
+	$projectsForAccount = Mite_Api::getResourcesByKeyValue('project', 
+		'customer_name', $miteCustomerName, $projects);
 	$data = array(
 		'brand' => $config['app']['title'],
 		'account_name' => $miteCustomerName,
 		'account_salutation' => $informalSalutation,
 		'account' => $customer,
-		'account_id' => $accountid);
+		'account_id' => $accountid,
+		'projects' => $projectsForAccount);
 	$app->render('account.twig', $data);
 })->name('account');
 ?>
--- app/routes/account_project.php
+++ app/routes/account_project.php
@@ -17,11 +17,15 @@ $app->get('/accounts/:accountid/projects/', function ($accountid) use ($app, $co
 //GET route for project by id
 $app->get('/accounts/:accountid/projects/:projectid/', function ($accountid, $projectid) use ($app, $config, $api) {
 	$miteCustomerName = $config['accounts'][$accountid]['mite_customer_name'];
+	$projects = json_decode($api->sendGetRequest('/projects.json'), true);
+	$projectsForAccount = Mite_Api::getResourcesByKeyValue('project', 
+		'customer_name', $miteCustomerName, $projects);
 	$project = json_decode($api->sendGetRequest('/projects/'.$projectid.'.json'), true);
 	$time_entries = json_decode($api->sendGetRequest('/time_entries.json?project_id='.$projectid), true);
 	$data = array(
 		'brand' => $config['app']['title'],
 		'project' => $project,
+		'projects' => $projectsForAccount,
 		'account_name' => $miteCustomerName,
 		'account_id' => $accountid,
 		'time_entries' => $time_entries);
